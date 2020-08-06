1ffb3b55e14e42d09f7b64351754982fd12ffb02
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-06 19:14:01 +0530

    start node app

---
+++ file.js
@@ -0,0 +1,10 @@
+exports.hello = function () {
+  return "hi";
+};
+exports.fullName = function fullName() {
+  return "gaurav palaspagar";
+};
+
+module.exports.log = function (msg) {
+  console.log(msg);
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+const helper= require('./file');
+console.log(`${helper.hello()} ${helper.fullName()} `) ;
+helper.log("nice")
+console.log(require.cache);
