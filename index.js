c1df1e56fdf98f7feb04009cde55f03aebdcd38d
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-07 23:34:59 +0530

    ad basic one

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const helper= require('./file');
-console.log(`${helper.hello()} ${helper.fullName()} `) ;
-helper.log("nice")
-console.log(require.cache);
+#!/usr/bin/env node
+const fs = require('fs');
+fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(filenames.toLocaleString());
+//   for (const fileName of filenames) {
+//       fs.lstat(fileName,(err, stats) => {
+//         if(err){
+//             throw new Error(err);
+//         }
+//         console.log(fileName,stats.isFile());
+
+//       });
+//   }
+});
+
+const lstat = function lstat(fileName){
+    return new Promise((resolve, reject) => {
+        fs.lstat(fileName,(err, stats) => {
+            if(err){
+                reject(err);
+            }
+            resolve(stats);
+        });        
+    });
+}
