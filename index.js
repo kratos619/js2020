90580c0f76f2847d3eda62aa68e52b118bb51615
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-10 15:44:09 +0530

    cli app to load file details

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 #!/usr/bin/env node
-const fs = require('fs');
-fs.readdir(process.cwd(), (err, filenames) => {
+const fs = require("fs");
+const chalk = require("chalk");
+const targetDir = process.argv[2] || process.cwd();
+const { lstat } = fs.promises;
+const path = require("path");
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(filenames.toLocaleString());
-//   for (const fileName of filenames) {
-//       fs.lstat(fileName,(err, stats) => {
-//         if(err){
-//             throw new Error(err);
-//         }
-//         console.log(fileName,stats.isFile());
 
-//       });
-//   }
+  const statPromis = filenames.map(function (fileName) {
+    return lstat(path.join(targetDir, fileName));
+  });
+
+  const allStats = await Promise.all(statPromis);
+
+  for (const stat of allStats) {
+    const index = allStats.indexOf(stat);
+
+    if (stat.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.blue(chalk.bold(filenames[index])));
+    }
+  }
 });
-
-const lstat = function lstat(fileName){
-    return new Promise((resolve, reject) => {
-        fs.lstat(fileName,(err, stats) => {
-            if(err){
-                reject(err);
-            }
-            resolve(stats);
-        });        
-    });
-}
