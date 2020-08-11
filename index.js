23b457f078d77a2e5ec7eb328cd1b173c65b961e
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-11 13:18:47 +0530

    complete

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 #!/usr/bin/env node
-const chokidar = require('chokidar');
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of the file to Execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error(`could not find the file ${name}`);
+    }
 
-chokidar
-  .watch(".")
-  .on("add", () => console.log("file added"))
-  .on("change", () => console.log("file change"))
-  .on("unlink", () => console.log("file unlink"));
+    let proc ;
+    const start = debounce(() => {
+      
+      if(proc){
+        proc.kill();
+      }
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add",  start)
+      .on("change",  start)
+      .on("unlink",  start);
+  });
+program.parse(process.argv);
