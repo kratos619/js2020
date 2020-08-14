5466fe3b7065a83da79795a552437e78606da1e2
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-14 15:22:19 +0530

    create fresh app-again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 @NgModule({
   declarations: [
-    ServerComponent,
     AppComponent
   ],
   imports: [
