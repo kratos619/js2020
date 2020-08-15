7ac1b85b61683fdf2c57628d1c03a3f8c8d898d1
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-15 23:52:10 +0530

    find most used word form paragraph

---
+++ mostUsedWord.js
@@ -0,0 +1,34 @@
+const string = `is simply dummy text of printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`;
+
+function findMostUsedWord(string) {
+  if (string == null) {
+    throw new Error("Enter pair of strings");
+  }
+  let maxUsedCount = 0;
+  let maxUsedWord = "";
+
+  let wordsArr = string.split(" ");
+  let wordSet = {};
+  for (const iterator of wordsArr) {
+    if (wordSet[iterator]) {
+      wordSet[iterator]++;
+    } else {
+      wordSet[iterator] = 1;
+    }
+  }
+  for (const key in wordSet) {
+    if (wordSet.hasOwnProperty(key)) {
+      console.log(wordSet[key]);
+      if (wordSet[key] > maxUsedCount) {
+        maxUsedCount = wordSet[key];
+        maxUsedWord = key;
+      }
+    }
+  }
+  return {
+    maxUsedWord,
+    maxUsedCount,
+  };
+}
+
+console.log(findMostUsedWord(string));
