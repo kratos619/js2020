24d4a7c6bbca656758741120fac31b4e8462baa6
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-16 18:09:44 +0530

    basic Structure

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredients} from '../shared/ingredients.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredients[] = [
+    new Ingredients('ing name',20),
+    new Ingredients('ing name 1',30),
+    new Ingredients('ing name 2',40)
+  ]
   constructor() { }
 
   ngOnInit(): void {
