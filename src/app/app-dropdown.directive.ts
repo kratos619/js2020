6df6bde58f5e526b453b18ee43f8f1ed4c58d918
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-19 11:30:04 +0530

    add directive

--- src/app/app-dropdown.directive.ts
+++ src/app/app-dropdown.directive.ts
@@ -1,14 +1,16 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appAppDropdown]'
 })
 export class AppDropdownDirective {
   @HostBinding('class.open')  isOpen = false;
-  @HostListener('click') function (){
-    this.isOpen = !this.isOpen;
-    console.log(this.isOpen);
+  @HostListener('document:click', ['$event']) function (event: Event){
+    // console.log(event.target);
+    // this.isOpen = !this.isOpen;
+    // console.log(this.elRef.nativeElement.contains(event.target));
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
 }
